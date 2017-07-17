f692c324be433c6f8ed9b47643329899cca101f7
= <=>
2017-07-17 08:38:58 +0200

    component movement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { UserButtonDirective } from './user-button.directive';
 import { ProductImageDirective } from './product-list/product-image.directive';
+import { ProductDetailsComponent } from './product-list/product-details/product-details.component';
 
 // import { ButtonsModule } from 'ngx-bootstrap';
 
@@ -27,7 +28,8 @@ import { ProductImageDirective } from './product-list/product-image.directive';
     CategoryListComponent,
     ProductListComponent,
     UserButtonDirective,
-    ProductImageDirective
+    ProductImageDirective,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-list/product-details/product-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Product } from '../../product'
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  @Input() product: Product;
+  constructor() { }
+  ngOnInit() {
+  }
+
+}

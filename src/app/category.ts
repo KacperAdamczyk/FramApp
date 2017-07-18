20c1f5b90e256e5d18804318519760f2cee249b9
= <=>
2017-07-18 15:42:21 +0200

    Small improvements

--- src/app/category.ts
+++ src/app/category.ts
@@ -1,10 +1,3 @@
 export class Category {
-  public id: string;
-  public title: string;
-  public description: string;
-  constructor(id = '', title = '', description = '') {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-  }
+  constructor(public id = '', public title = '', public description = '') {}
 }
--- src/app/product.ts
+++ src/app/product.ts
@@ -1,30 +1,12 @@
 export class Product {
-  id_real: string;
-  id: number;
-  title: string;
-  description: string;
-  category: string;
-  imgUrl: string;
-  promoted: boolean;
-  price: number;
-  amount: number;
-  constructor(id_real = '',
-              id = 0,
-              title = '',
-              description = '',
-              category = '',
-              imgUrl = '',
-              promoted = false,
-              price = 0,
-              amount = 0) {
-    this.id_real = id_real;
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.category = category;
-    this.imgUrl = imgUrl;
-    this.promoted = promoted;
-    this.price = price;
-    this.amount = amount;
+  constructor(public id_real = '',
+              public id = 0,
+              public title = '',
+              public description = '',
+              public category = '',
+              public imgUrl = '',
+              public promoted = false,
+              public price = 0,
+              public amount = 0) {
   }
 }
